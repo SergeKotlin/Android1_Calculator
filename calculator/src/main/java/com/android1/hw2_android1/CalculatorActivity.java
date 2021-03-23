3d2e86f03456c072ca56446d37bfe9b244e6224a
Serge <jobsfolder@yandex.ru>
2021-03-23 21:30:36 +0300

    add picture to the app backround and try custom picture.. (tasks for Home Work 2 from Android1)

--- calculator/src/main/java/com/android1/hw2_android1/CalculatorActivity.java
+++ calculator/src/main/java/com/android1/hw2_android1/CalculatorActivity.java
@@ -3,6 +3,7 @@ package com.android1.hw2_android1;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        ConstraintLayout calc_layout = findViewById(R.id.calculator_layout_id);
+        calc_layout.setBackgroundResource(R.drawable.ic_bender_xml);
+//        calc_layout.setBackgroundResource(R.drawable.ic_launcher_background);
     }
 }
