3ad3240bbdcc9fd74b0c3dc5753c71b566f9c7fe
Serge <jobsfolder@yandex.ru>
2021-03-30 09:24:57 +0300

    @SergeKotlin add Lesson 4 (from Android1)

---
+++ calculator/src/main/java/com/android1/hw2_android1/Lesson4_Android1_Activity.java
@@ -0,0 +1,15 @@
+package com.android1.hw2_android1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lesson4_Android1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson4_activity);
+    }
+
+}
