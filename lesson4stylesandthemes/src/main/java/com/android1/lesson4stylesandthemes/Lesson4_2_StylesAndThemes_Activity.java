521cf1eef8c5a765daa31e47586428a33ecc220f
Serge <jobsfolder@yandex.ru>
2021-04-06 23:07:47 +0300

    up Lesson 4.2 StylesAndThemes (from Android1)

--- lesson4stylesandthemes/src/main/java/com/android1/lesson4stylesandthemes/Lesson4_2_StylesAndThemes_Activity.java
+++ lesson4stylesandthemes/src/main/java/com/android1/lesson4stylesandthemes/Lesson4_2_StylesAndThemes_Activity.java
@@ -9,6 +9,7 @@ public class Lesson4_2_StylesAndThemes_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lesson4_2_activity);
+//        setContentView(R.layout.lesson4_2_activity);
+        setContentView(R.layout.lesson4_2_activity2);
     }
 }
